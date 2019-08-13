348f9e216fbbf23266120c04a0055a39c01afb78
caio <caiomarcos@sigmais.com.br>
2019-08-13 17:27:53 -0300

    max and min

--- mlx.py
+++ mlx.py
@@ -7,9 +7,19 @@ print("init")
 ser = serial.Serial('COM25', 921600)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
-cmd = True
+value = float(sio.read(5))
+mini = 30
+maxi = 30
 
-while(cmd):
-	# line = sio.read(4607)
-	values = float(sio.read(5))
-	print(values)
+while(True):
+	cmd = 768
+	while(cmd):
+		# line = sio.read(4607)
+		value = float(sio.read(5))
+		if value < mini:
+			mini = value
+		if value > maxi:
+			maxi = value
+		cmd = cmd - 1
+	print(maxi)
+	print(mini)
